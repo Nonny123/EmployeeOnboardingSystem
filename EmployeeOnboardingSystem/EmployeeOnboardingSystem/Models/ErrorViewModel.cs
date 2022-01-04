a57efe60b392874f0542e20238d5cdefcd32ae06
Nonso <justcoolk@yahoo.com>
2022-01-04 09:26:40 +0100

    Created the solution files

---
+++ EmployeeOnboardingSystem/EmployeeOnboardingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeOnboardingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
