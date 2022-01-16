d0f2602cfc9277200006209247fe607663b89d7d
Nonso <justcoolk@yahoo.com>
2022-01-16 16:12:13 +0100

    Infrastructure - Create IMail interface

---
+++ EmployeeOnboardingSystem/OnBoarding.Infrastructure/Interfaces/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using Onboarding.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Infrastructure.Interfaces
+{
+    public interface IMailService
+    {
+        public Task SendEmailAsync(EmailRequest emailRequest);
+    }
+}
