733bfafc4d1cad5b4326d7c9b9ffe602c63807ab
Nonso <justcoolk@yahoo.com>
2022-01-19 11:57:30 +0100

    Implement Infrastructure ISlackservice

---
+++ EmployeeOnboardingSystem/OnBoarding.Infrastructure/Interfaces/ISlackService.cs
@@ -0,0 +1,17 @@
+﻿using OnBoarding.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Infrastructure.Interfaces
+{
+    public interface ISlackService
+    {
+        HttpWebResponse PostMessage(string channel, string message);
+        Task<Response<string>> SendMessageToChannelWithSMB(string channel, string text);
+        Task SlackChannel();
+    }
+}
