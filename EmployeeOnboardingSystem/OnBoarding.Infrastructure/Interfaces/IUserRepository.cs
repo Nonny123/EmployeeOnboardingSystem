7ce986b2e563328b38ae82312b77ae32e9314ba3
Nonso <justcoolk@yahoo.com>
2022-01-20 06:16:31 +0100

    Create Infrastructure IUserRepository

---
+++ EmployeeOnboardingSystem/OnBoarding.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Onboarding.Domain.Entities;
+using OnBoarding.Infrastructure.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Infrastructure.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task SaveAsync();
+        Task<IEnumerable<User>> SearchUserAsync(string searchTerm);
+        string UpdateUserAvatar(User user);
+        Task<User> GetUserByEmail(string email);
+    }
+   
+}
