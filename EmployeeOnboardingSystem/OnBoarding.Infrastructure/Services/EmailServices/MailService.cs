4482725bfe328af1aa7330a9c0cd2e841e4264c7
Nonso <justcoolk@yahoo.com>
2022-01-29 07:06:15 +0100

    Create Infrastructure MailService

---
+++ EmployeeOnboardingSystem/OnBoarding.Infrastructure/Services/EmailServices/MailService.cs
@@ -0,0 +1,61 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using Onboarding.Domain.Entities;
+using OnBoarding.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Infrastructure.Services.EmailServices
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(EmailRequest emailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailRequest.ToEmail));
+            email.Subject = emailRequest.Subject;
+
+            if (emailRequest.CcEmail != null)
+                email.Cc.Add(MailboxAddress.Parse(emailRequest.CcEmail));
+
+            if (emailRequest.BccEmail != null)
+                email.Bcc.Add(MailboxAddress.Parse(emailRequest.BccEmail));
+
+            var builder = new BodyBuilder();
+            if (emailRequest.Attachments != null)
+            {
+                byte[] fileBytes;
+                foreach (var file in emailRequest.Attachments)
+                {
+                    if (file.Length > 0)
+                    {
+                        using (var ms = new MemoryStream())
+                        {
+                            file.CopyTo(ms);
+                            fileBytes = ms.ToArray();
+                        }
+                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                    }
+                }
+            }
+            builder.HtmlBody = emailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ EmployeeOnboardingSystem/OnBoarding.Infrastructure/Services/EmailServices/MailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Infrastructure.Services.EmailServices
+{
+    public class MailSettings
+    {
+        public string Mail { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
