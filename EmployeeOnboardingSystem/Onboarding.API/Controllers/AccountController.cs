38cc274287bfef3e87f52bf25b755bdf65a6c935
Nonso <justcoolk@yahoo.com>
2022-02-02 07:16:50 +0100

    Create API project, API AccountController

---
+++ EmployeeOnboardingSystem/Onboarding.API/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onboarding.Domain.DTOs;
+using OnBoarding.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onboarding.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] UserLogin model)
+        {
+            var response = await _accountService.LoginAsync(model);
+            if (response.Status == "Failed")
+            {
+                return Unauthorized(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("forgot-password")]
+        public async Task<IActionResult> ForgotPasswordAsync(string email)
+        {
+            var response = await _accountService.ForgotPasswordAsync(email);
+            if (response.Status == "Failed")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("reset-password")]
+        public async Task<IActionResult> ResetPasswordAsync([FromBody] ResetPassword model)
+        {
+            var response = await _accountService.ResetPasswordAsync(model);
+            if (!ModelState.IsValid && response.Status == "Failed")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
--- EmployeeOnboardingSystem/Onboarding.Domain/DTOs/UserUpdateProfileDto.cs
+++ EmployeeOnboardingSystem/Onboarding.Domain/DTOs/UserUpdateProfileDto.cs
@@ -1,13 +1,50 @@
 ﻿using Onboarding.Domain.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 namespace Onboarding.Domain.DTOs
 {
-    public class UserUpdateProfileDto : UserBase 
+    public class UserUpdateProfileDto
     {
-        public DateTime LastModified { get; set; }
+        [MaxLength(50, ErrorMessage = "First Name can not be longer than 50 characters")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Middle Name can not be longer than 50 characters")]
+        public string MiddleName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Last Name can not be longer than 50 characters")]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+        [Phone]
+        [MaxLength(20, ErrorMessage = "Phone number can not be longer than 50 characters")]
+        public string PhoneNumber { get; set; }
+
+        [Phone]
+        [MaxLength(20, ErrorMessage = "Phone number can not be longer than 50 characters")]
+        public string PhoneNumber2 { get; set; }
+
+        [MaxLength(50)]
+        public string ResidentialAddress { get; set; }
+
+        [MaxLength(50)]
+        public string CityOfResidence { get; set; }
+
+        [MaxLength(50)]
+        public string StateofResidence { get; set; }
+
+        [MaxLength(50)]
+        public string StateofOrigin { get; set; }
+
+        [MaxLength(50)]
+        public string CountryOfOrigin { get; set; }
+
+        public string DateOfBirth { get; set; }
+
     }
 }
