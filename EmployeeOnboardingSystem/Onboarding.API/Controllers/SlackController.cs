23001611d014b09e9a5b09e49c6fe37e94fb96ba
Nonso <justcoolk@yahoo.com>
2022-02-06 05:37:37 +0100

    Create API SlackController

---
+++ EmployeeOnboardingSystem/Onboarding.API/Controllers/SlackController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnBoarding.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onboarding.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SlackController : ControllerBase
+    {
+        private readonly ISlackService _slackService;
+
+        public SlackController(ISlackService slackService)
+        {
+            _slackService = slackService;
+        }
+
+        [HttpPost("new-message")]
+        public async Task<IActionResult> SendMessageToAChanneWithSMB(string channel, string text)
+        {
+            var result = await _slackService.SendMessageToChannelWithSMB(channel, text);
+            return StatusCode(result.StatusCode, result);
+        }
+
+    }
+}
