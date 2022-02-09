1036d298977bcd31f0a12841a270198575cf839a
Nonso <justcoolk@yahoo.com>
2022-02-09 19:15:02 +0100

    Create API IdentityServiceExtension

---
+++ EmployeeOnboardingSystem/Onboarding.API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Onboarding.Domain.Entities;
+using OnBoarding.Infrastructure.AppDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onboarding.API.Extensions
+{
+    public static class IdentityServiceExtension
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 8;
+                options.SignIn.RequireConfirmedEmail = true;
+            });
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            builder.AddEntityFrameworkStores<OnBoardingDbContext>()
+                .AddDefaultTokenProviders();
+        }
+    }
+}
